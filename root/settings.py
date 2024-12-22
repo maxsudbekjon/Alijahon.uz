86698018c57632ee4daf8401e6e040ccc9e967b6
maxsudbekjon <toshpulatovm73@gmail.com>
2024-12-22 22:54:11 +0500

    Initial commit

--- root/settings.py
+++ root/settings.py
@@ -69,12 +69,8 @@ AUTH_USER_MODEL = 'apps.User'
 
 DATABASES = {
     'default': {
-        'ENGINE': os.getenv("DB_ENGINE"),
-        'NAME': os.getenv("DB_NAME"),
-        'USER': os.getenv("DB_USER"),
-        'PASSWORD': os.getenv("DB_PASSWORD"),
-        'HOST': os.getenv("DB_HOST"),
-        'PORT': os.getenv("DB_PORT"),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
